b7c7f69ed977c2710e3d47a2bd35b1937c08edb8
林千里 <lincheney@gmail.com>
2024-02-21 16:17:12 +1100

    make exec-filter compatible with exec

--- _tbl/exec_filter.py
+++ _tbl/exec_filter.py
@@ -5,8 +5,12 @@ class exec_filter(exec_):
     ''' filter rows using python '''
     name = None
     parser = argparse.ArgumentParser()
+    parser.set_defaults(slurp=False)
     parser.add_argument('script')
-    parser.add_argument('-E', '--remove-errors', action='store_true')
+    parser.add_argument('-q', '--quiet', action='store_true')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-I', '--ignore-errors', action='store_true')
+    group.add_argument('-E', '--remove-errors', action='store_true')
 
     def __init__(self, opts):
         opts.script = [f'if not ({opts.script}): row = None']
