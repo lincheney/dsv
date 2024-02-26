36fe6ec36f54d9d2900b58210f8a2620e612f7d8
林千里 <lincheney@gmail.com>
2024-02-27 03:54:59 +1100

    add a flag to indicate ifs is plain string

--- _tbl/_base.py
+++ _tbl/_base.py
@@ -83,7 +83,7 @@ class _Base:
     def determine_delimiters(self, line):
         opts = self.opts
         if opts.ifs:
-            if re.escape(opts.ifs) != opts.ifs:
+            if re.escape(opts.ifs) != opts.ifs and not opts.plain_ifs:
                 opts.ifs = re.compile(opts.ifs)
 
         else:
