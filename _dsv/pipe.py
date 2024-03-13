fa99f85cec545d83d59fa48c327dc7cebbcc7ac5
林千里 <lincheney@gmail.com>
2024-03-13 17:52:36 +1100

    pipe: allow appending columns

--- _dsv/pipe.py
+++ _dsv/pipe.py
@@ -3,14 +3,17 @@ import subprocess
 import threading
 from collections import deque
 from ._base import _Base
+from ._utils import utf8_type
 from ._column_slicer import _ColumnSlicer
 
 class pipe(_ColumnSlicer):
     ''' pipe rows through a processs '''
-    parser = argparse.ArgumentParser(add_help=False)
+    parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('-k', '--fields', action='append', default=[])
     parser.add_argument('-x', '--complement', action='store_true')
-    parser.add_argument('command', nargs='*')
+    parser.add_argument('-a', '--append-columns', action='append', default=[], type=utf8_type)
+    parser.add_argument('command', nargs='+')
+    parser.add_argument('-q', '--no-quote-output', action='store_false', dest='quote_output')
 
     def __init__(self, opts):
         super().__init__(opts)
@@ -33,12 +36,27 @@ class pipe(_ColumnSlicer):
         for stdout, is_header in _Base(opts).process_file(proc.stdout, do_yield=True, do_callbacks=False):
             row = self.queue.popleft()
 
-            # write the stdout back into the original row
-            indices = self.slice(list(range(len(row))), self.opts.complement)
-            for k, v in zip(indices, stdout):
-                row[k] = v
+            if self.opts.append_columns:
+                left = len(self.header or row)
+                extra = len(self.opts.append_columns)
+
+                newrow = row[:min(left, len(row))]
+                newrow += [b''] * (left - len(newrow))
+                newrow += stdout
+                newrow += [b''] * (left + extra - len(newrow))
+                newrow += row[left:]
+                row = newrow
+
+            else:
+                # write the stdout back into the original row
+                indices = self.slice(list(range(len(row))), self.opts.complement)
+                for k, v in zip(indices, stdout):
+                    row[k] = v
 
             super().on_row(row)
+
+    def on_header(self, header):
+        super().on_header(header + self.opts.append_columns)
 
     def on_row(self, row):
         input = self.slice(row, self.opts.complement)
