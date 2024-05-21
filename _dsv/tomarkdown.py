4162452346c1a8369f2897582142ba072edffb5f
林千里 <lincheney@gmail.com>
2024-05-21 18:29:06 +1000

    fix tomarkdown

--- _dsv/tomarkdown.py
+++ _dsv/tomarkdown.py
@@ -3,22 +3,26 @@ from ._base import _Base
 class tomarkdown(_Base):
     ''' convert to markdown table '''
 
+    def __init__(self, opts):
+        super().__init__(opts)
+        self.rows = []
+
     def on_header(self, header):
         self.on_row(header)
 
     def on_row(self, row):
         row = [col.replace(b'\\', b'\\\\').replace(b'|', b'\\|').replace(b'`', b'\\`') for col in row]
-        self.gathered_rows.append(self.format_columns(row, None, None, quote_output=False) + [b''])
+        self.rows.append(self.format_columns(row, None, None, quote_output=False) + [b''])
 
     def on_eof(self):
-        if not self.gathered_rows:
+        if not self.rows:
             return
         if self.opts.no_header:
-            self.gathered_rows.insert([b''] * len(self.gathered_rows[0]))
-        padding = self.justify(self.gathered_rows)
+            self.rows.insert([b''] * len(self.rows[0]))
+        padding = self.justify(self.rows)
 
         self.opts.ofs = b' | '
-        for i, (p, row) in enumerate(zip(padding, self.gathered_rows)):
+        for i, (p, row) in enumerate(zip(padding, self.rows)):
             self.outfile.write(b'| ')
             super().on_row(row, p)
 
