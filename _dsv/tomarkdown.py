5577d10fb391c3770063af0febd41ccbee0f0ca1
林千里 <lincheney@gmail.com>
2024-05-22 17:27:59 +1000

    tomarkdown: fix handling of quoting

--- _dsv/tomarkdown.py
+++ _dsv/tomarkdown.py
@@ -3,6 +3,8 @@ from ._base import _Base
 class tomarkdown(_Base):
     ''' convert to markdown table '''
 
+    ofs = b' | '
+
     def __init__(self, opts):
         super().__init__(opts)
         self.rows = []
@@ -12,7 +14,7 @@ class tomarkdown(_Base):
 
     def on_row(self, row):
         row = [col.replace(b'\\', b'\\\\').replace(b'|', b'\\|').replace(b'`', b'\\`') for col in row]
-        self.rows.append(self.format_columns(row, None, None, quote_output=False) + [b''])
+        self.rows.append(self.format_columns(row, self.ofs, self.opts.ors, quote_output=self.opts.quote_output) + [b''])
 
     def on_eof(self):
         if not self.rows:
@@ -21,7 +23,10 @@ class tomarkdown(_Base):
             self.rows.insert([b''] * len(self.rows[0]))
         padding = self.justify(self.rows)
 
-        self.opts.ofs = b' | '
+        # rows are already quoted
+        self.opts.quote_output = False
+
+        self.opts.ofs = self.ofs
         for i, (p, row) in enumerate(zip(padding, self.rows)):
             self.outfile.write(b'| ')
             super().on_row(row, p)
