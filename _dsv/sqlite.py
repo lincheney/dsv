8ef70de40a32e0e46f83aeb979c83a906874c9cb
林千里 <lincheney@gmail.com>
2024-12-03 03:37:01 +1100

    make sqlite work with pipeline

--- _dsv/sqlite.py
+++ _dsv/sqlite.py
@@ -13,14 +13,9 @@ class sqlite(_Base):
     def __init__(self, opts):
         super().__init__(opts)
         self.proc = None
-        if self.opts.header == 'yes':
-            self.parser.error('Cannot use sqlite without a header')
 
     def start_proc(self):
         if not self.proc:
-            if self.opts.header != 'yes':
-                self.parser.error('Cannot use sqlite without a header')
-
             self.proc = subprocess.Popen([
                 'sqlite3', '-csv', '-header',
                 '-separator', self.DELIM,
@@ -29,15 +24,22 @@ class sqlite(_Base):
             ], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     def on_header(self, header):
+        self.start_proc()
         return self.on_row(header)
 
     def on_row(self, row):
-        self.start_proc()
+        if not self.proc:
+            self.parser.error('Cannot use sqlite without a header')
         self.proc.stdin.write(self.DELIM.join(self.format_columns(row, self.DELIM, b'\n', True)) + b'\n')
 
     def on_eof(self):
         if self.proc:
             self.proc.stdin.close()
             self.opts.ifs = self.DELIM
-            list(_Base(self.opts).process_file(self.proc.stdout))
+
+            child = _Base(self.opts)
+            child.on_row = super().on_row
+            child.on_header = super().on_header
+            list(child.process_file(self.proc.stdout))
+
             self.proc.wait()
