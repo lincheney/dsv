dc446776d2fd43ed77c2814b80f49ec7f4fe1f0f
林千里 <lincheney@gmail.com>
2025-08-26 13:45:51 +1000

    expose errors on pipeline

--- src/subcommands/_pipeline.rs
+++ src/subcommands/_pipeline.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Result, Context};
 use std::sync::mpsc;
 use crate::base::*;
 use clap::{Parser, Subcommand};
@@ -17,12 +17,14 @@ enum Args {
 }
 
 pub struct Handler {
+    err_receiver: mpsc::Receiver<Result<()>>,
 }
 
 impl Handler {
     pub fn new(opts: Opts, base: &mut Base, is_tty: bool) -> Result<Self> {
         let Args::Args(args) = opts.args;
 
+        let (err_sender, err_receiver) = mpsc::channel();
         let (opts_sender, opts_receiver) = mpsc::channel();
         for (i, arg) in args.rsplit(|a| a == "!").enumerate() {
             let new_sender = base.sender.clone();
@@ -33,20 +35,41 @@ impl Handler {
             let opts_sender = opts_sender.clone();
             let arg = arg.to_owned();
             let scope = base.scope;
+            let err_sender = err_sender.clone();
             scope.spawn(move || {
-                let (handler, mut base) = super::Subcommands::from_args(&arg, new_sender, scope, is_tty)?;
-                // take opts from the last handler?
-                if last {
-                    opts_sender.send(base.opts.clone()).unwrap();
-                }
-                handler.forward_messages(&mut base, receiver)
+                let result = (|| {
+                    let (handler, mut base) = super::Subcommands::from_args(&arg, new_sender, scope, is_tty)?;
+                    // take opts from the last handler?
+                    if last {
+                        opts_sender.send(base.opts.clone()).unwrap();
+                    }
+                    handler.forward_messages(&mut base, receiver)
+                })();
+                err_sender.send(result).unwrap();
             });
         }
 
         base.opts = opts_receiver.recv().unwrap();
 
-        Ok(Self {})
+        Ok(Self {
+            err_receiver
+        })
     }
 }
 
-impl Processor for Handler {}
+impl Processor for Handler {
+    fn on_eof(self, base: &mut Base) -> Result<bool> {
+        base.on_eof()?;
+
+        let mut result = Ok(());
+        for err in self.err_receiver.iter() {
+            if result.is_ok() {
+                result = result.and(err);
+            } else if let Err(e) = err {
+                result = result.context(e);
+            }
+        }
+        result?;
+        Ok(false)
+    }
+}
