307d6f5f5a3d0cd3604ca1a32d0346d018e09926
林千里 <lincheney@gmail.com>
2025-09-29 23:47:07 +1000

    update completion

--- completions/dsv.zsh
+++ completions/dsv.zsh
@@ -1022,6 +1022,7 @@ _shtab__dsv_xargs_options=(
   "--no-tag[don\'t tag lines with the input rows]"
   {-k,--column}"[new header column name]:column:"
   {-I,--replace-str}"[use the replacement string instead of \{\}]:replace_str:"
+  "--stdin[input to command]:stdin:"
   {-H,--header}"[treat first row as a header]"
   {-N,--no-header}"[do not treat first row as header]"
   "--drop-header[do not print the header]"
