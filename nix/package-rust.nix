79a90fe05008c0bfabb89e1776d052f95f640834
Tim Quelch <tim@tquelch.com>
2025-11-19 19:33:11 +1100

    Ensure python available for py subcommand

--- nix/package-rust.nix
+++ nix/package-rust.nix
@@ -1,8 +1,9 @@
 {
   installShellFiles,
   rustPlatform,
-  python3Packages,
-  shtab ? python3Packages.shtab,
+  autoPatchelfHook,
+  python3,
+  shtab ? python3.pkgs.shtab,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "dsv";
@@ -13,7 +14,13 @@ rustPlatform.buildRustPackage (finalAttrs: {
   nativeBuildInputs = [
     shtab
     installShellFiles
+    autoPatchelfHook
   ];
+
+  # force python as lib dependency for autoPatchelf
+  # this ensures python is available for `dsv py`
+  buildInputs = [ python3 ];
+  runtimeDependencies = [ python3 ];
 
   postInstall = ''
     bash ./make-completions.sh
